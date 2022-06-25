e29f2a57f67118cb10ac37a33af3f7a565b76324
Cagatay Yilmaz <cagatay.y@outlook.com.tr>
2022-06-26 01:07:10 +0300

    Navbar has been created responsively

--- src/modules/Navbar.js
+++ src/modules/Navbar.js
@@ -2,6 +2,19 @@ import React from "react";
 
 export default function Navbar() {
     return(
-        <div>HAYDAR BAŞ</div>
+        <nav className="Navbar">
+            <div className="logo">BURNTILLMOON</div>
+            <ul>
+                <li>TOKENS</li>
+                <li>TOP-LIST</li>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+            </ul>
+            <div className="menu">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+        </nav>
     )
 }
