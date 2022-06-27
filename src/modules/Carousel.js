b3e1aea4572938f1b23e57b20faadcf253aec667
Cagatay Yilmaz <cagatay.y@outlook.com.tr>
2022-06-27 22:09:45 +0300

    Carousel buttons have been added

--- src/modules/Carousel.js
+++ src/modules/Carousel.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Carousel(){
     return(
         <div className="carousel-container">
+            <div id="prevButtonContainer">
+                <FontAwesomeIcon icon={faAngleLeft} size="2x" />
+            </div>
             <div className="carousel">
                 <div className="token"></div>
                 <div className="token"></div>
@@ -11,6 +16,9 @@ export default function Carousel(){
                 <div className="token"></div>
                 <div className="token"></div>
             </div>
+            <div id="nextButtonContainer">
+                <FontAwesomeIcon icon={faAngleRight} size="2x" />
+            </div> 
         </div>
     )
 }
