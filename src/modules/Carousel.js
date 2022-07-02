9dbd5be7c18101ab2c824d854537c8f10e7376fd
Cagatay Yilmaz <cagatay.y@outlook.com.tr>
2022-07-02 17:42:32 +0300

    Carousel Slider working properly

--- src/modules/Carousel.js
+++ src/modules/Carousel.js
@@ -2,50 +2,86 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const tokens = document.getElementsByClassName("token");
-
-let counter = 0;
+const tokenGroups = document.getElementsByClassName("tokenGroup");
+let counter = -80;
+console.log(counter/80)
 
 function next(){
-    counter += 80;
-
-    for(let i = 0; i < 3; i++){
-        tokens.parentNode.insertBefore(tokens[tokens.length-i], tokens.parentNode[0]);
-        console.log("loop worked")
-    }
+        console.log(counter/80)
+        if(counter/80 >= 0) return;
+        counter += 80;
+        for(let i = 0; i < tokenGroups.length; i++){
+            tokenGroups[i].style.transform = "translateX(" + counter + "vw)";
+            tokenGroups[i].style.transition = "1s";
+        }
     
-    for(let i = 0; i < 3; i++){
-        tokens.parentNode.removeChild(tokens[tokens.length - i]);
+}
+
+function prev(){
+        console.log(counter/80)
+        if(counter/80 <= -tokenGroups.length +1) return;
+        counter -= 80;
+        for(let i = 0; i < tokenGroups.length; i++){
+        tokenGroups[i].style.transform = "translateX(" + counter + "vw)";
+        tokenGroups[i].style.transition = "1s";
+        }
     }
 
-    for(let i = 0; i < tokens.length; i++){
-        tokens[i].style.transform = "translate(" + counter + "vw,0)";
+
+function transitionEnd(){
+    
+    if(tokenGroups[Math.abs(counter/80)].id === "lastClone"){
+        counter = -80;
+        for(let i = 0; i < tokenGroups.length; i++){
+            tokenGroups[i].style.transition = "none";    
+        }
+        for(let i = 0; i < tokenGroups.length; i++){
+            tokenGroups[i].style.transform = "translateX(" + counter + "vw)";
+        }
+    }
+    if(tokenGroups[Math.abs(counter/80)].id === "firstClone"){
+        counter = -160;
+        for(let i = 0; i < tokenGroups.length; i++){
+            tokenGroups[i].style.transition = "none";    
+        }
+        for(let i = 0; i < tokenGroups.length; i++){
+            tokenGroups[i].style.transform = "translateX(" + counter + "vw)";
+        }
     }
 }
-function prev(){
-    counter -= 80;
-    for(let i = 0; i < tokens.length; i++){
-        tokens[i].style.transform = "translate(" + counter + "vw,0)"
-    }
-}
+
 
 
 export default function Carousel(){
     return(
         <div className="carousel-container">
             <div id="prevButtonContainer">
-                <FontAwesomeIcon icon={faAngleLeft} size="2x" onClick={prev} />
+                <FontAwesomeIcon icon={faAngleLeft} size="2x" onClick={next} />
             </div>
             <div className="carousel">
-                <div className="token">token1</div>
-                <div className="token">token2</div>
-                <div className="token">token3</div>
+            <div className="tokenGroup" id="firstClone">
                 <div className="token">token4</div>
                 <div className="token">token5</div>
                 <div className="token">token6</div>
             </div>
+                <div className="tokenGroup" id="tokenGroup1">
+                    <div className="token">token1</div>
+                    <div className="token">token2</div>
+                    <div className="token">token3</div>
+                </div>
+                <div className="tokenGroup" id="tokenGroup2">
+                    <div className="token">token4</div>
+                    <div className="token">token5</div>
+                    <div className="token">token6</div>
+                </div>
+                <div className="tokenGroup" id="lastClone" onTransitionEnd={transitionEnd}>
+                    <div className="token">token1</div>
+                    <div className="token">token2</div>
+                    <div className="token">token3</div>
+                </div>    
+            </div>
             <div id="nextButtonContainer">
-                <FontAwesomeIcon icon={faAngleRight} size="2x" onClick={next}/>
+                <FontAwesomeIcon icon={faAngleRight} size="2x" onClick={prev}/>
             </div> 
         </div>
     )
