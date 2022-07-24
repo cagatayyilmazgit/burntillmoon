6e57a6700733b68e3922caf32fa323fcc53a0b05
Cagatay Yilmaz <cagatay.y@outlook.com.tr>
2022-07-24 17:47:37 +0300

    useEffect working properly

--- src/modules/Carousel.js
+++ src/modules/Carousel.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import Tokens from "./Token";
 import axios from "axios";
 import tokenInfo from "../tokens.json"
 
-
 const tokenGroups = document.getElementsByClassName("tokenGroup");
 let counter = -80;
-
+//#region Carousel
 function next(){
         if(counter/80 >= 0) return;
         counter += 80;
@@ -50,19 +49,33 @@ function transitionEnd(){
         }
     }
 }  
-
+//#endregion
 
 export default function Carousel(){
        
-    const [burnedBSW, setBurnedBSW] = useState();
+    // function getBurnedTokenAmount (tokensName) {
+    //     let token = tokensName.result
+    //     let result = axios.get('https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=' + tokenInfo.token[0] + '&address=' + tokenInfo.token[1] + '&apikey=' + tokenInfo.token[2])
+    //     .then(res=>(res.data.result.slice(0,res.data.result.length - 18)),[])
+    //     return result;
+    // };
+    const tokenName = ['bsw','quack','SFM'];
+    const [burnedAmounts, setBurnedAmount] = useState([]);
+    
+    
+    useEffect(() => {
+        function getBurnedTokenAmount(){
+            for(let i = 0; i < tokenName.length; i++){
+                let token = tokenInfo[`${tokenName[i]}`][0];
+                axios.get('https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=' + token.contractaddress + '&address=' + token.address + '&apikey=' + token.apikey).then(res => setBurnedAmount(prevArray => [...prevArray, res.data.result]));   
+            }
+        }
+        getBurnedTokenAmount();
+    },[])
+        
+    
 
-    useEffect((contract) => {
-        // BSW
-        contract = tokenInfo.bsw[0].contractaddress;
-        axios
-            .get('https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=' + contract + '&address=0x000000000000000000000000000000000000dead&apikey=IAFC45SXZ1G5J3D85S3VJJ4Q7USKK38CEM')
-            .then(res=>{setBurnedBSW(res.data.result.slice(0,res.data.result.length - 18))})
-    }, [])
+    console.log()
 
     return(
         <div className="carousel-container">
@@ -72,12 +85,20 @@ export default function Carousel(){
             <div className="carousel">
             <div className="tokenGroup" id="firstClone">
                 <Tokens 
-                image="bsw" 
-                burnedPercentage = "0"
+                image="bsw.png" 
+                burnedPercentage = {String(burnedAmounts[0]).slice(0,9).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 dailyBurnRate = "0,001" 
                 price = "0,31"/>
-                <Tokens image="avax.png" burnedPercentage = "59" dailyBurnRate = "0,001" price = "0,31"/>
-                <div className="token">token6</div>
+                <Tokens 
+                image="quack.png" 
+                burnedPercentage = {String(burnedAmounts[1]).slice(0,17).replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 
+                dailyBurnRate = "0,001" 
+                price = "0,31"/>
+                <Tokens 
+                image="sfm.png" 
+                burnedPercentage = {String(burnedAmounts[2]).slice(0,12).replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 
+                dailyBurnRate = "0,001" 
+                price = "0,31"/>
             </div>
                 <div className="tokenGroup" id="tokenGroup1">
                     <div className="token">token1</div>
@@ -85,9 +106,21 @@ export default function Carousel(){
                     <div className="token">token3</div>
                 </div>
                 <div className="tokenGroup" id="tokenGroup2">
-                    <Tokens image="bsw.png" burnedPercentage = {burnedBSW} dailyBurnRate = "0,001" price = "0,31"/>
-                    <Tokens image="avax.png" burnedPercentage = "59" dailyBurnRate = "0,001" price = "0,31"/>
-                    <div className="token">token6</div>
+                    <Tokens 
+                    image="bsw.png" 
+                    burnedPercentage = {String(burnedAmounts[0]).slice(0,9).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    dailyBurnRate = "0,001" 
+                    price = "0,31"/>
+                    <Tokens 
+                    image="quack.png" 
+                    burnedPercentage = {String(burnedAmounts[1]).slice(0,17).replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 
+                    dailyBurnRate = "0,001" 
+                    price = "0,31"/>
+                    <Tokens 
+                    image="sfm.png" 
+                    burnedPercentage = {String(burnedAmounts[2]).slice(0,12).replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 
+                    dailyBurnRate = "0,001" 
+                    price = "0,31"/>
                 </div>
                 <div className="tokenGroup" id="lastClone" onTransitionEnd={transitionEnd}>
                     <div className="token">token1</div>
--- src/modules/Token.js
+++ src/modules/Token.js
@@ -4,7 +4,7 @@ export default function Tokens(props){
     return(
     <div className="token">
     <img src={`/images/${props.image}`} className="tokenImage"></img>
-    <div>%{props.burnedPercentage} BURNED</div>
+    <div>{props.burnedPercentage} <span>BURNED</span></div>
     <div>AVERAGE DAILY BURN RATE - {props.dailyBurnRate}</div>
     <div>PRICE - {props.price}$</div>
     <div>TOP TEN HOLDER PERCENTAGE - {props.topTenHolder}</div>
